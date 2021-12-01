c91fc48e3abe6bc21a6ea314a79b6f6a819360f2
Dave Handy <dave.handy@datadoghq.com>
2021-12-01 12:13:45 -0500

    2021-01

---
+++ build.sbt
@@ -0,0 +1,4 @@
+name := "advent-of-code"
+version := "0.1"
+scalaVersion := "3.1.0"
+idePackagePrefix := Some("com.handy")
---
+++ src/main/scala/aoc2021/Day1.scala
@@ -0,0 +1,29 @@
+package com.handy.aoc2021
+
+import scala.io.Source
+import scala.util.Using
+
+object Day1 {
+
+  @main def part1(): Unit = {
+    Using.resource(Source.fromResource("com/handy/aoc2021/day1.txt", getClass.getClassLoader)) {
+      source =>
+        val num = source.getLines().map(_.toInt).sliding(2, 1).count(s => s(1) > s.head)
+        println(num)
+    }
+  }
+
+  @main def part2(): Unit = {
+    Using.resource(Source.fromResource("com/handy/aoc2021/day1.txt", getClass.getClassLoader)) {
+      source =>
+        val num = source
+          .getLines()
+          .map(_.toInt)
+          .sliding(3, 1)
+          .map(_.sum)
+          .sliding(2, 1)
+          .count(s => s(1) > s.head)
+        println(num)
+    }
+  }
+}
